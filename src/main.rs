3ee9f3d39d9eacd0bcc52cd9973f181cf819b7b9
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2024-05-19 12:41:48 +0200

    feat: use clap derive

--- src/main.rs
+++ src/main.rs
@@ -6,73 +6,64 @@ use std::sync::mpsc::{Receiver, RecvError, Sender};
 use std::thread::{sleep, spawn};
 use std::time::Duration;
 
-use clap::{Arg, arg, Command as ClapCommand};
-use go_parse_duration::parse_duration;
+use clap::{Arg, arg, Command as ClapCommand, Parser};
+use go_parse_duration::parse_duration as go_parse_duration;
 use libc::{kill, pid_t, SIGTERM};
-use log::{info, LevelFilter};
+use log::{error, info, LevelFilter};
 use notify::{RecommendedWatcher, RecursiveMode, Watcher};
 use simplelog::{ColorChoice, CombinedLogger, Config, TerminalMode, TermLogger};
 
-fn exit<V>(status_code: i32, msg: String) -> V {
-    print!("{}", msg);
-    process::exit(status_code);
+#[derive(Parser, Debug)]
+#[command(name = "rpod", about, long_about = None)]
+struct Cli {
+    #[arg(required = true, help = "Path to binary file")]
+    path: String,
+
+    #[arg(
+        short = 'd',
+        help = "Delay when restarting the binary after an unexpected exit",
+        default_value = "3s",
+        value_parser = parse_duration
+    )]
+    restart_delay: Duration,
+
+    #[arg(last = true, help = "Additional arguments to pass to the binary")]
+    binary_args: Vec<String>
 }
 
-fn main() {
+fn parse_duration(v: &str) -> Result<Duration, String> {
+    match go_parse_duration(v) {
+        Ok(v) => Ok(Duration::from_nanos(v as u64)),
+        Err(err) => Err(format!("{:?}", err)),
+    }
+}
+
+fn init_logger() {
     CombinedLogger::init(
         vec![
             TermLogger::new(LevelFilter::Info, Config::default(), TerminalMode::Mixed, ColorChoice::Auto),
         ]
     ).unwrap();
+}
+
+
+fn main() {
+    let args = Cli::parse();
+    init_logger();
 
     info!("Starting...");
-    let matches = ClapCommand::new("rpod")
-        .bin_name("rpod")
-        .args([
-            arg!(restart_delay: -d <DELAY>)
-                .help("Delay when restarting the binary after an unexpected exit")
-                .default_value("3s"),
-            Arg::new("path")
-                .help("Path to binary file")
-                .required(true),
-            arg!(binary_args: [BINARY_ARGS])
-                .num_args(1..)
-                .last(true),
-        ])
-        .get_matches();
-
-    let binary_path = match matches.get_one::<String>("path") {
-        None => panic!("Should not happen, arg 'path' is required"),
-        Some(v) => v,
-    }.clone();
-    info!("Detect binary: {:?}", binary_path);
-
-    let restart_delay = match matches.get_one::<String>("restart_delay") {
-        None => Duration::from_secs(3),
-        Some(v) => match parse_duration(v) {
-            Ok(v) => Duration::from_nanos(v as u64),
-            Err(err) => exit(1, format!("{:?}", err)),
-        },
-    };
-    info!("Restart delay: {:}s", restart_delay.as_secs());
-
-    let binary_args = match matches.get_many::<String>("binary_args") {
-        None => vec![],
-        Some(v) => v.map(|v| v.clone()).collect(),
-    };
-    info!("Detect binary args: {:?}", binary_args);
-
-    ProcessExecutor::new(
-        ProcessConfiguration{
-            path: binary_path,
-            args: Some(binary_args),
-            restart_delay: Some(restart_delay),
+    let option = ProcessExecutor::new(
+        ProcessConfiguration {
+            path: args.path,
+            args: Some(args.binary_args),
+            restart_delay: Some(args.restart_delay),
         },
         Arc::new(NotifyFileWatcher::new())
     ).start();
-
-    info!("Wait signal...");
-    loop {}
+    if option.is_some() {
+        error!("{}", option.unwrap());
+        process::exit(1);
+    }
 }
 
 #[derive(Clone)]
